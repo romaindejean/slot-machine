4ae223e6759c07d3f45cbdf5b0a0e0d5301ae5e8
Romain DEJEAN <romain.dejean@gadz.org>
2017-02-16 22:37:50 -0800

    added container to slots

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 $(document).ready(function() {
   
+  let finalPositionOne = '';
+  let finalPositionTwo = '';
+  let finalPositionThree = '';
+  let hasWon = '';
 
   function findFinalPosition(slotNumber) {
     let finalSlotPosition = Math.ceil(Math.random()*3);
     $('#' + slotNumber).removeClass(slotNumber + '-running').addClass(slotNumber + '-stopping-' + finalSlotPosition);
+    return finalSlotPosition;
+  }
+
+  function verifyWin(positionOne, positionTwo, positionThree) {
+    if (positionOne === positionTwo && positionOne === positionThree) {
+      hasWon = true;
+      $('.caffeine').removeClass('hidden');
+    } else {
+      hasWon = false;
+      $('.no-caffeine').removeClass('hidden');
+    }
   }
 
   $('#get-caffeine').on('click', function() {
     if (!$('#slot-one').hasClass('slot-one-running')) {
+      hasWon = '';
+      $('.no-caffeine').addClass('hidden');
+      $('.caffeine').addClass('hidden');
       $('#slot-one').removeClass('slot-one-stopping-1 slot-one-stopping-2 slot-one-stopping-3').addClass('slot-one-running');
       $('#slot-two').removeClass('slot-two-stopping-1 slot-two-stopping-2 slot-two-stopping-3').addClass('slot-two-running');
       $('#slot-three').removeClass('slot-three-stopping-1 slot-three-stopping-2 slot-three-stopping-3').addClass('slot-three-running');
     } else {
-      findFinalPosition('slot-one');
-      findFinalPosition('slot-two');
-      findFinalPosition('slot-three');
+      finalPositionOne = findFinalPosition('slot-one');
+      finalPositionTwo = findFinalPosition('slot-two');
+      finalPositionThree = findFinalPosition('slot-three');
+      verifyWin(finalPositionOne, finalPositionTwo, finalPositionThree);
     }
   });
 
