715c0a6f4f1ecd7005246de5c22b328ba629fdb8
Romain DEJEAN <romain.dejean@gadz.org>
2017-02-17 11:54:42 -0800

    Improved semantics

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ $(document).ready(() => {
     finalPositionReelOne = '';
     finalPositionReelTwo = '';
     finalPositionReelThree = '';
-    //Hidding result message
+    // Hidding result message
     $('.no-caffeine').addClass('hidden');
     $('.caffeine').addClass('hidden');
-    //Removing stopping class for each reel
+    // Removing stopping class for each reel
     $('#reel-one').removeClass('reel-one-stopping-1 reel-one-stopping-2 reel-one-stopping-3');
     $('#reel-two').removeClass('reel-two-stopping-1 reel-two-stopping-2 reel-two-stopping-3');
     $('#reel-three').removeClass('reel-three-stopping-1 reel-three-stopping-2 reel-three-stopping-3');
-    //Removing reels border style
+    // Removing reels border style
     $('.reels-wrapper').removeClass('reels-wrapper-border-win reels-wrapper-border-loose');
   }
 
@@ -36,16 +36,16 @@ $(document).ready(() => {
   }
 
   const verifyWin = (positionOne, positionTwo, positionThree) => {
-    //The user gets his caffeine only is the three reels have a matching position
+    // The user gets his caffeine only is the three reels have a matching position
     if (positionOne === positionTwo && positionOne === positionThree) {
       assignBeverage(positionOne);
-      //Adding a delay to synchronize with the slow down animation
+      // Adding a delay to synchronize with the slow down animation
       setTimeout(() => {
         $('.caffeine').removeClass('hidden');
         $('.reels-wrapper').addClass('reels-wrapper-border-win');
       }, 1000)
     } else {
-       //Adding a delay to synchronize with the slow down animation
+       // Adding a delay to synchronize with the slow down animation
       setTimeout(() => {
         $('.no-caffeine').removeClass('hidden');
         $('.reels-wrapper').addClass('reels-wrapper-border-loose');
@@ -53,21 +53,20 @@ $(document).ready(() => {
     }
   }
 
-// this solution provide an automatic stop of the spinning
     $('#get-caffeine').on('click', () => {
       reInitializeState();
       const button = $('#get-caffeine');
 
-      //Disabling spinning button 
+      // Disabling spinning button 
       $(button).prop("disabled", true);
       $(button).addClass('button-get-caffeine-disabled');
 
-      //Starting reels spinning
+      // Starting reels spinning
       $('#reel-one').addClass('reel-one-running');
       $('#reel-two').addClass('reel-two-running');
       $('#reel-three').addClass('reel-three-running');
 
-      //Stopping reels spinning after 1sec
+      // Stopping reels spinning after 1sec
       setTimeout(() => {
         finalPositionReelOne = assignFinalPosition('reel-one');
         finalPositionReelTwo = assignFinalPosition('reel-two');
@@ -75,7 +74,7 @@ $(document).ready(() => {
         verifyWin(finalPositionReelOne, finalPositionReelTwo, finalPositionReelThree);
       }, 1000);
 
-      //1sec for the running time + 1sec for the slow down animation time
+      // 1sec for the running time + 1sec for the slow down animation time
       setTimeout(() => {
         $(button).removeClass('button-get-caffeine-disabled');
         $(button).prop("disabled", false);
