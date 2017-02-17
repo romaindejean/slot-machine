6fd1102fea5b5acc0a305edc8dbaa61a150e99d1
Romain DEJEAN <romain.dejean@gadz.org>
2017-02-17 14:09:59 -0800

    Refactored result message

--- app.js
+++ app.js
@@ -3,20 +3,25 @@ $(document).ready(() => {
   let finalPositionReelOne = '';
   let finalPositionReelTwo = '';
   let finalPositionReelThree = '';
+
+  // Caching DOM elements
   const resultMessage = $('.result-message');
+  const getCaffeineButton = $('#get-caffeine');
+  const reelsWrapper = $('.reels-wrapper');
+
 
   const reInitializeState = () => {
     finalPositionReelOne = '';
     finalPositionReelTwo = '';
     finalPositionReelThree = '';
     // Hidding result message
-    resultMessage.addClass('hidden')
+    resultMessage.addClass('hidden');
     // Removing stopping class for each reel
     $('#reel-one').removeClass('reel-one-stopping-1 reel-one-stopping-2 reel-one-stopping-3');
     $('#reel-two').removeClass('reel-two-stopping-1 reel-two-stopping-2 reel-two-stopping-3');
     $('#reel-three').removeClass('reel-three-stopping-1 reel-three-stopping-2 reel-three-stopping-3');
     // Removing reels border style
-    $('.reels-wrapper').removeClass('reels-wrapper-border-win reels-wrapper-border-loose');
+    reelsWrapper.removeClass('reels-wrapper-border-win reels-wrapper-border-loose');
   }
 
   const assignFinalPosition = reelNumber => {
@@ -30,7 +35,6 @@ $(document).ready(() => {
     position === 1 ? 'coffee' :
     position === 2 ? 'tea' :
     position === 3 ? 'expresso' : null;
-    // caffeineMessage.html(`Yay caffeine! Enjoy your ${beverage}...`);
     resultMessage.html(`Yay caffeine! Enjoy your ${beverage}...`);
   }
 
@@ -38,30 +42,29 @@ $(document).ready(() => {
     // The user gets his caffeine only is the three reels have a matching position
     if (positionOne === positionTwo && positionOne === positionThree) {
       assignBeverage(positionOne);
+      resultMessage.addClass('caffeine');
       // Adding a delay to synchronize with the slow down animation
       setTimeout(() => {
-        resultMessage.removeClass('hidden')
-        resultMessage.addClass('caffeine')
-        $('.reels-wrapper').addClass('reels-wrapper-border-win');
-      }, 1000)
+        resultMessage.removeClass('hidden');
+        reelsWrapper.addClass('reels-wrapper-border-win');
+      }, 1000);
     } else {
+      resultMessage.addClass('no-caffeine');
+      resultMessage.html('Sorry no caffeine for you today!');
        // Adding a delay to synchronize with the slow down animation
       setTimeout(() => {
-        resultMessage.removeClass('hidden')
-        resultMessage.addClass('no-caffeine')
-        resultMessage.html('Sorry no caffeine for you today!');
-        $('.reels-wrapper').addClass('reels-wrapper-border-loose');
-      }, 1000)
+        resultMessage.removeClass('hidden');
+        reelsWrapper.addClass('reels-wrapper-border-loose');
+      }, 1000);
     }
   }
 
-    $('#get-caffeine').on('click', () => {
+    getCaffeineButton.on('click', () => {
       reInitializeState();
-      const button = $('#get-caffeine');
 
       // Disabling spinning button 
-      button.prop("disabled", true);
-      button.addClass('button-get-caffeine-disabled');
+      getCaffeineButton.prop("disabled", true);
+      getCaffeineButton.addClass('button-get-caffeine-disabled');
 
       // Starting reels spinning
       $('#reel-one').addClass('reel-one-running');
@@ -78,8 +81,8 @@ $(document).ready(() => {
 
       // 1sec for the running time + 1sec for the slow down animation time
       setTimeout(() => {
-        button.removeClass('button-get-caffeine-disabled');
-        button.prop("disabled", false);
+        getCaffeineButton.removeClass('button-get-caffeine-disabled');
+        getCaffeineButton.prop("disabled", false);
       }, 2000);
       
   });
