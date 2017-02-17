4ee553648231ab8653b14901c6f1bb70b6be1f7d
Romain DEJEAN <romain.dejean@gadz.org>
2017-02-16 16:37:45 -0800

    Refactored stopping class assignment

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
 
+  function findFinalPosition(slotPosition) {
+    let finalPositionSlot = Math.ceil(Math.random()*3);
+    $('#' + slotPosition).removeClass(slotPosition + '-running').addClass(slotPosition + '-stopping-' + finalPositionSlot);
+  }
+
   $('#get-caffeine').on('click', function() {
-    $('#slot-1').toggleClass('slot-1-running');
-    $('#slot-2').toggleClass('slot-2-running');
-    $('#slot-3').toggleClass('slot-3-running');
+    if (!$('#slot-1').hasClass('slot-1-running')) {
+      $('#slot-1').removeClass('slot-1-stopping-1 slot-1-stopping-2 slot-1-stopping-3').addClass('slot-1-running');
+      $('#slot-2').removeClass('slot-2-stopping-1 slot-2-stopping-2 slot-2-stopping-3').addClass('slot-2-running');
+      $('#slot-3').removeClass('slot-3-stopping-1 slot-3-stopping-2 slot-3-stopping-3').addClass('slot-3-running');
+    } else {
+      findFinalPosition('slot-1');
+      findFinalPosition('slot-2');
+      findFinalPosition('slot-3');
+    }
   });
 
 });
